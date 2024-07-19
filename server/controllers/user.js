5230bb8d23def85dedd44bd5a227c7356a85db8e
harunaabaldeh <harunaabaldeh@gmail.com>
2024-07-19 23:50:31 +0100

    moved auth login to controller

---
+++ server/controllers/user.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const { Users } = require("../models");
+const bcrypt = require("bcrypt");
+const { sign } = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+  const { username, password } = req.body;
+  bcrypt.hash(password, 10).then((hash) => {
+    Users.create({
+      username: username,
+      password: hash,
+    });
+    res.json("SUCCESS");
+  });
+};
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+  const user = await Users.findOne({ where: { username: username } });
+  if (!user) {
+    res.json({ error: "User does not exist" });
+  } else {
+    bcrypt.compare(password, user.password).then((match) => {
+      if (!match) {
+        res.json({ error: "Wrong username or password" });
+      }
+
+      const accessToken = sign(
+        { username: user.username, id: user.id },
+        "super_secret_token"
+      );
+
+      res.json(accessToken);
+    });
+  }
+};
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,39 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { Users } = require("../models");
-const bcrypt = require("bcrypt");
-const { sign } = require("jsonwebtoken");
+const auth = require("../controllers/user");
 
-router.post("/register", async (req, res) => {
-  const { username, password } = req.body;
-  bcrypt.hash(password, 10).then((hash) => {
-    Users.create({
-      username: username,
-      password: hash,
-    });
-    res.json("SUCCESS");
-  });
-});
+router.post("/register", auth.register);
 
-router.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-  const user = await Users.findOne({ where: { username: username } });
-  if (!user) {
-    res.json({ error: "User does not exist" });
-  } else {
-    bcrypt.compare(password, user.password).then((match) => {
-      if (!match) {
-        res.json({ error: "Wrong username or password" });
-      }
-
-      const accessToken = sign(
-        { username: user.username, id: user.id },
-        "super_secret_token"
-      );
-
-      res.json(accessToken);
-    });
-  }
-});
+router.post("/login", auth.login);
 
 module.exports = router;
