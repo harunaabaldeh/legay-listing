a778d4924697fa73790fcb1d6660e95d96211e04
harunaabalde <harunaabaldeh@gmail.com>
2024-08-11 11:25:12 +0100

    update application page

--- client/src/pages/SubmitApplication.jsx
+++ client/src/pages/SubmitApplication.jsx
@@ -1,7 +1,57 @@
-const SubmitApplication = () => {
-  return (
-    <div>SubmitApplication</div>
-  )
-}
+import axios from "axios";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
 
-export default SubmitApplication
+const SubmitApplication = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [resume, setResume] = useState("");
+  const [date, setDate] = useState();
+  const params = useParams();
+
+  const submitApplication = () => {
+    const data = {
+      name: name,
+      email: email,
+      resume: resume,
+      data: date,
+    };
+
+    axios
+      .post(`http://localhost:9000/${params.id}/applications`, data)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="resume"
+        onChange={(e) => setResume(e.target.value)}
+      />
+      <input
+        type="date"
+        placeholder="date"
+        onChange={(e) => setDate(e.target.value)}
+      />
+      <button onClick={submitApplication}>Submit</button>
+    </div>
+  );
+};
+
+export default SubmitApplication;
